bef5a1f16bc94853118009254cedf018f087304c
Ahmed <sultan_902002@yahoo.com>
2019-09-02 11:12:55 -0600

    implement dayListItem function and styling

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "components/DayListItem.scss";
+let classnames = require('classnames');
+
+export default function DayListItem(props) {
+  const formatSpots = (value) => {
+    if (value === 0) {
+      return "no spots remaining";
+    }
+    if (value === 1) {
+      return "1 spot remaining";
+    }
+    if (value === 2) {
+      return "2 spots remaining";
+    }
+  }
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
