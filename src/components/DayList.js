0d10a8cfc027efe3705643d429e61155ce0339ea
Ahmed <sultan_902002@yahoo.com>
2019-09-02 15:27:25 -0600

    add dayList component to render dayListItem components

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+
+export default function DayList(props) {
+  const days = props.days;
+  const dayList = days.map((day) => (
+    <DayListItem
+      key={day.name}
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  ));
+
+  return (
+    <ul>{dayList}</ul>
+  );
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -13,11 +13,20 @@ export default function DayListItem(props) {
     if (value === 2) {
       return "2 spots remaining";
     }
+    if (value === 3) {
+      return "3 spots remaining";
+    }
+    if (value === 4) {
+      return "4 spots remaining";
+    }
+    if (value === 5) {
+      return "5 spots remaining";
+    }
   }
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
-  });
+  }); 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
