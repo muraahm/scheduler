7a701694e671d5277ac9bac43a9a09edf7594e05
Ahmed <sultan_902002@yahoo.com>
2019-09-03 10:10:50 -0600

    modify appointment component to display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "components/Appointment/header";
+import Show from "components/Appointment/show";
+import Empty from "components/Appointment/empty";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+
   return (
-    <article className="appointment"></article>
-  );
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} />: <Empty />}
+    </article>
+  )
 }
