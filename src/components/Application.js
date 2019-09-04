3379fa56fe6ce2d84eb989b07f609445839aab31
Ahmed <sultan_902002@yahoo.com>
2019-09-04 11:37:33 -0600

    fetching appointments data and interview

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,93 +3,43 @@ import axios from "axios";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import DayList from "./DayList"
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Ahmed Murad",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png"
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "2pm",
-    interview: {
-      student: "Someone",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Someone2",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  }
-];
 
 export default function Application(props) {
-  // const [day, setDay] = useState("Monday");
-  // const [days, setDays] = useState([]);
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState({ ...state, days });
 
-
-  const appointmentsList = appointments.map((appointment) => (
-    <Appointment key={appointment.id}
-      id={appointment.id}
-      time={appointment.time}
-      interview={appointment.interview}
-    />
-  ));
+  const appointments = getAppointmentsForDay(state, state.day)
+  const appointmentsList = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />)
+  });
   useEffect(
     () => {
-    axios.get('/api/days')
-      .then((response) => {
-        setState(prev => ({ ...prev, days: response.data }));
-        // setDays(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+      Promise.all([
+        axios.get('/api/days'),
+        axios.get('/api/appointments'),
+        axios.get('/api/interviewers')
+      ])
+        .then((all) => {
+          setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+        })
+        .catch(function (error) {
+        });
+    }, []);
+    // console.log(state.interviewers)
   return (
     <main className="layout">
       <section className="sidebar">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,23 @@
 export function getAppointmentsForDay(state, day) {
+  let results = []
   const selectedDay = state.days.find(item => item.name === day);
-  // console.log(selectedDays)
   if (state.days.length === 0 || selectedDay === undefined) {
-    return []
+    return results
   }
   const returnedArray = selectedDay.appointments.map(id => state.appointments[id])
-  
-  // console.log(returnedArray)
   return returnedArray;
+}
+
+
+export  function getInterview(state, interview) {
+  // console.log(state.appointments["2"].interview)
+  // console.log(interview.interviewers)
+  if (!interview) {
+    return null
+  }
+  const intervirewersObj = Object.values(state.interviewers)
+  const selectedInterview = intervirewersObj.find(interviewer => interviewer.id === interview.interviewer);
+  const result = {student : interview.student, interviewer: selectedInterview}
+  
+  return result;
 }
